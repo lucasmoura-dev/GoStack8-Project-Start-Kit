26523a41786cb2acab1d46ddbf3a51412bf81cae
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-09 22:04:44 -0300

    Adicionado o sequelize e sequelize-cli. Configurações do sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,13 @@
 /* Credenciais para acessar o banco de dados */
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true, // gera automaticamente as datas de edição e criação
+    underscored: true, // define que irá utilizar o padrão de tabelas e colunas usando underlines
+    underscoredAll: true, // underscore nas colunas
+  },
+};
